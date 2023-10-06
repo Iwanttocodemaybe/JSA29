914ac8d08e000ca7fa78b3b9dfc73b166a319566
Iwanttocodemaybe <so31032008@gmail.com>
2023-10-06 20:46:05 +0700

    new commit

---
+++ Day 1/app.js
@@ -0,0 +1,17 @@
+let first_name = "Hoàng"
+let middle_name = " Nguyên "
+let last_name = "Sơn"
+let full_name = first_name + middle_name + last_name
+let age = 15
+console.log(" Tên tôi là " + full_name + " năm nay tôi " + age + " tuổi")
+
+let a = 42
+let c = 60
+let b = 53
+// let S = h + (a * b)/2
+// console.log("Diện tích hình thang là " + S)
+
+let p = (a + b + c) / 2
+let S = p * (p - a) * (p - b) * (p - c)
+S = Math.sqrt(S)
+console.log("Diện tích tam giác là " + 90S)
